65855b508d38dd6558fd42ebddf73e160eda2ffa
gilju <giljus>
2018-10-12 09:35:04 +0900

    springboot 2.0.5 web starter

---
+++ src/main/java/com/example/study_springboot_2/StudySpringboot2Application.java
@@ -0,0 +1,12 @@
+package com.example.study_springboot_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringboot2Application.class, args);
+    }
+}
